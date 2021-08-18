ccedec16f383442870f4e48d2ab927cad2cd4281
PauloSanRibeiro <PauloSanRibeiro>
2021-08-17 21:11:52 -0300

    Exception Delete

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -71,8 +71,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Can't delete seller because he/she has sales" });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
